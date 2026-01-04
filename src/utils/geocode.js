e00cd1f8e54514f824d04272e079e6e151cc1597
mcastroal <marilyn.castroalonso@gmail.com>
2026-01-04 16:50:59 -0500

    first commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/search/geocode/v6/forward?q=' + encodeURIComponent(address) + '&access_token=pk.eyJ1IjoibWNhc3Ryb2FsIiwiYSI6ImNtanh3YmNlYTZkanozZ3B3emJwd292YnMifQ.Rh9KqTQD7ljOizs6vM2u5Q'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].geometry.coordinates[1],
+                longitude: body.features[0].geometry.coordinates[0],
+                location: body.features[0].properties.full_address
+            })
+        }
+    })
+}
+
+module.exports = geocode
