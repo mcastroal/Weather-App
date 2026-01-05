6e1395f80e93d0e8e468652c8c1511595680094a
mcastroal <marilyn.castroalonso@gmail.com>
2026-01-04 20:10:09 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location + '&units=' + units).then((response) => {
+    fetch('/weather?address=' + location + '&units=' + units).then((response) => {
         response.json().then((data) => {
             if (data.error){
                 messageOne.textContent = data.error
